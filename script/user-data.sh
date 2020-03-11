67454af2afea45934357e2d04703758eae289889
surjan-kshetri <36111454+surjan-kshetri@users.noreply.github.com>
2020-03-11 19:12:32 -0400

    Update user-data.sh

--- script/user-data.sh
+++ script/user-data.sh
@@ -1,5 +1,31 @@
 
-
+vol=vol=vol-0b52b8f26ac4dcda9
+mount_dir=/surjan
+device=/dev/xvdf/
+if [ ! -d $mount_dir ]
+then 
+mkdir $mount_dir 
+fi 
+echo "this command is to fetch instance id"
+instance_id=`curl http://169.254.169.254/latest/meta-data/instance-id`
+echo "this command is to attach EBS to EC2"
+aws ec2 attach-volume --volume-id $vol --instance-id $instance_id --device /dev/sdf
+sleep 60 
+echo "mount filesystem to folder" 
+mount $device $mount_dir
+mount_dir=/surjan
+device=/dev/xvdf/
+if [ ! -d $mount_dir ]
+then 
+mkdir $mount_dir 
+fi 
+echo "this command is to fetch instance id"
+instance_id=`curl http://169.254.169.254/latest/meta-data/instance-id`
+echo "this command is to attach EBS to EC2"
+aws ec2 attach-volume --volume-id $vol --instance-id $instance_id --device /dev/sdf
+sleep 60 
+echo "mount filesystem to folder" 
+mount $device $mount_dir
 
 
 
